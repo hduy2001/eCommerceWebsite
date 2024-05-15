237e889b5aba852872995347f0a3f7700e9170e8
Hoang Duy <nhduy151001@gmail.com>
2024-05-15 11:42:11 +0700

    hello controller commit

---
+++ src/main/java/com/duy/assignment/eCommerceWebsitereview/rest/HelloController.java
@@ -0,0 +1,12 @@
+package com.duy.assignment.eCommerceWebsitereview.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Duy";
+    }
+}
---
+++ src/test/java/com/duy/assignment/eCommerceWebsitereview/ECommerceWebsiteReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duy.assignment.eCommerceWebsitereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebsiteReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
